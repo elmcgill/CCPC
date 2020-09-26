8faf93b4fab86bb78d80ad7de56f2d09ef195403
elmcgill <elmcgill@iastate.edu>
2020-09-26 10:45:43 -0500

    Setup basic navigation

--- ccpc-frontend/src/App.js
+++ ccpc-frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/homepage';
+import NotFoundPage from './components/pagenotfound';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <Switch>
+       <Route path='/' component={Home} exact />
+       <Route component={NotFoundPage} />
+    </Switch>
   );
 }
 
--- ccpc-frontend/src/index.js
+++ ccpc-frontend/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BroswerRouter, BrowserRouter, Route, Switch} from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   rootElement
 );
 
