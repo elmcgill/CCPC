2b6422a014763d53ee21f29c50354fc2cc6e78fd
elmcgill <elmcgill@iastate.edu>
2020-09-26 10:07:47 -0500

    Setup the middlewares and connected to a local database

--- ccpc-backend/server.js
+++ ccpc-backend/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const bodyparser = require('body-parser');
+const cors = require('cors');
+
+dotenv.config();
 
 const app = express();
 const port = 5000;
+
+//Connect to the database with mongoose
+mongoose.connect(`mongodb://${process.env.DB_IP}/ccpc_db`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Setup middleware
+app.use(bodyparser.json());
+app.use(cors());
 
 //Super simple get request
 app.get('/', (req, res) => {
